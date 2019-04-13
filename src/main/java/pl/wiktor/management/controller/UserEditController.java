b8a7d545c84f002daf410e2f3faf6f846bcadd5a
Wiktor Krzyzanowski <wiktor.krzyzanowski1@gmail.com>
2019-04-13 14:45:57 +0200

    Labels to edit user window

--- src/main/java/pl/wiktor/management/controller/UserEditController.java
+++ src/main/java/pl/wiktor/management/controller/UserEditController.java
@@ -1,10 +1,13 @@
 package pl.wiktor.management.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+import pl.wiktor.management.model.UserBO;
 import pl.wiktor.management.service.AppContext;
 import pl.wiktor.management.utils.StageManager;
 
@@ -15,9 +18,25 @@ public class UserEditController {
     public AnchorPane window;
     @FXML
     public Label userLabel;
+    @FXML
+    public TextField firstNameLabel;
+    @FXML
+    public TextField lastNameLabel;
+    @FXML
+    public TextField emailLabel;
+    @FXML
+    public TextField roleLabel;
+    @FXML
+    public Button submitButton;
+    @FXML
+    public Button saveButton;
+
+
+    private UserBO userToEdit;
 
     private final StageManager stageManager;
     private final AppContext appContext;
+
 
     public UserEditController(@Lazy StageManager stageManager, AppContext appContext) {
         this.stageManager = stageManager;
@@ -28,5 +47,10 @@ public class UserEditController {
     void initialize() {
         stageManager.fadeInAnimation(window);
         this.userLabel.setText(this.appContext.getUserToEdit().toString());
+        this.userToEdit = this.appContext.getUserToEdit();
+        this.firstNameLabel.setText(userToEdit.getFirstName());
+        this.lastNameLabel.setText(userToEdit.getLastName());
+        this.emailLabel.setText(userToEdit.getEmail());
+
     }
 }
